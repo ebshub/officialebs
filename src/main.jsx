ee78095cd3562f5340f9de28674edfef4cd9b902
ebshub <ebsebsebs567@gmail.com>
2024-04-25 01:20:06 -0400

    add assets and some folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page";
+import Book, { loader as bookLoader }from "./routes/book"
+// import Root, { loader as rootLoader } from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+},
+{
+    path: "books/:bookId",
+    loader: bookLoader,
+    element: <Book />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
